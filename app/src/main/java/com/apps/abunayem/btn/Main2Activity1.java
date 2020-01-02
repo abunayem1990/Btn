fa387afaef9c749cb1a958160e09ccfde91eebb4
MUHAMMAD ABU NAYEM <56333679+abunayem1990@users.noreply.github.com>
2020-01-02 09:33:52 +0600

    SSS

---
+++ app/src/main/java/com/apps/abunayem/btn/Main2Activity1.java
@@ -0,0 +1,8 @@
+package com.apps.abunayem.btn;
+
+/**
+ * Created by User on 7/26/2017.
+ */
+
+interface Main2Activity1 {
+}
---
+++ app/src/main/java/com/apps/abunayem/btn/Main3Activity.java
@@ -0,0 +1,8 @@
+package com.apps.abunayem.btn;
+
+/**
+ * Created by User on 7/26/2017.
+ */
+
+interface Main3Activity extends Main2Activity1 {
+}
